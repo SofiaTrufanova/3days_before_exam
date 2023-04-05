613c5f70ff14cd2b2cd451411dc8ad3cdb0bef75
Sofia Trufanova <nchestnov@dummydomain.com>
2023-04-06 01:41:17 +0300

    добавлена музыка и картинка в тестовый файл, в основных изменений нет

--- visual.py
+++ visual.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from pygame import mixer
 import text
 
 click_index = 0
@@ -8,7 +9,7 @@ day = 0
 is_luck = False
 
 
-def final():
+def final(event):
     global click_index
     label_text.configure(text=text.key_situations[-1])
     main_text.configure(text=text.main_text[click_index])
@@ -16,12 +17,16 @@ def final():
     if click_index == 19:
         label_text.configure(text='Конец игры.')
         main_text.grid_remove()
-        next_button.grid_remove()
+        window.bind("<Button-1>", do_nothing)
 
 
-def next_click():
+def do_nothing(event):
+    pass
+
+
+def next_click(event):
     global click_index, lucky_index, study_index, day
-    next_button.grid_remove()
+    window.bind("<Button-1>", do_nothing)
     main_text.configure(text=text.main_text[click_index])
     click_index += 1
     if click_index >= 16:
@@ -29,8 +34,7 @@ def next_click():
         second_study_button.grid_remove()
         first_lucky_button.grid_remove()
         second_lucky_button.grid_remove()
-        next_button.configure(command=final)
-        next_button.grid()
+        window.bind("<Button-1>", final)
     else:
         if is_luck:
             first_study_button.grid(column=0, row=4)
@@ -50,7 +54,7 @@ def first_study_click():
     main_text.configure(text=text.study_choices[study_index * 2])
     is_luck = False
     study_index += 1
-    next_button.grid(column=2, row=4)
+    window.bind("<Button-1>", next_click)
     first_study_button.configure(text=text.first_study_button[study_index])
     second_study_button.configure(text=text.second_study_button[study_index])
     first_study_button.grid_remove()
@@ -62,7 +66,7 @@ def second_study_click():
     main_text.configure(text=text.study_choices[study_index * 2 + 1])
     is_luck = False
     study_index += 1
-    next_button.grid(column=2, row=4)
+    window.bind("<Button-1>", next_click)
     first_study_button.configure(text=text.first_study_button[study_index])
     second_study_button.configure(text=text.second_study_button[study_index])
     first_study_button.grid_remove()
@@ -74,7 +78,7 @@ def first_lucky_click():
     main_text.configure(text=text.lucky_choices[lucky_index * 2])
     is_luck = True
     lucky_index += 1
-    next_button.grid(column=2, row=4)
+    window.bind("<Button-1>", next_click)
     first_lucky_button.configure(text=text.first_lucky_button[lucky_index])
     second_lucky_button.configure(text=text.second_lucky_button[lucky_index])
     first_lucky_button.grid_remove()
@@ -86,7 +90,7 @@ def second_lucky_click():
     main_text.configure(text=text.lucky_choices[lucky_index * 2 + 1])
     is_luck = True
     lucky_index += 1
-    next_button.grid(column=2, row=4)
+    window.bind("<Button-1>", next_click)
     first_lucky_button.configure(text=text.first_lucky_button[lucky_index])
     second_lucky_button.configure(text=text.second_lucky_button[lucky_index])
     first_lucky_button.grid_remove()
@@ -95,20 +99,28 @@ def second_lucky_click():
 
 window = Tk()
 window.title('3 дня до экзамена...')
-window.geometry('650x500')
+window.geometry('2560x1600')
+window["bg"] = "pink"
 label_text = Label(window, text='3 дня до экзамена...')
 main_text = Label(window, text='Начать игру:')
-next_button = Button(window, text='Дальше ->', command=next_click)
+window.bind("<Button-1>", next_click)
 first_study_button = Button(window, command=first_study_click, text=text.first_study_button[study_index])
 first_lucky_button = Button(window, command=first_lucky_click, text=text.first_lucky_button[lucky_index])
 second_study_button = Button(window, command=second_study_click, text=text.second_study_button[study_index])
 second_lucky_button = Button(window, command=second_lucky_click, text=text.second_lucky_button[lucky_index])
+bg = PhotoImage(file='small.png')
+kpm = Label(window, image=bg)
 
+mixer.init()
+mixer.music.load('everlasting-summer.mp3')
+
+mixer.music.play(-1)
+
+kpm.grid(column=0, row=6)
 label_text.grid(column=2, row=0)
 main_text.grid(column=2, row=2)
 first_study_button.grid(column=0, row=4)
 second_study_button.grid(column=4, row=4)
 first_study_button.grid_remove()
 second_study_button.grid_remove()
-next_button.grid(column=2, row=4)
 window.mainloop()
